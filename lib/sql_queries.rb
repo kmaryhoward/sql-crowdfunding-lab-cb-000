6ebe6d38ee1ae21def710f3aa1c0ea0d12bc9f05
Kathleen Howard <kmaryhoward@gmail.com>
2019-07-21 18:58:20 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -24,7 +24,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 "SELECT projects.title, pledges.amount - projects.funding_goal AS difference FROM projects
 INNER JOIN pledges ON projects.id = pledges.project_id
 GROUP BY projects.title
-HAVING difference > 1;"
+HAVING difference >= 0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
